17f28ff6478fe2c0c584d60c5e5a9d3a026d0537
Matej Babiak <9matej.babiak@gmail.com>
2025-06-07 21:55:39 +0200

    Improve product app - update, delete + styling

--- online_store/product/views.py
+++ online_store/product/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 from product.models import Product
 from product.forms import ProductForm
 from django.urls import reverse_lazy
 
-class Products(ListView):
+class ProductDetail(DetailView):
     model = Product
     template_name = 'product.html'
+    context_object_name = 'product'
+
+class Products(ListView):
+    model = Product
+    template_name = 'products.html'
     context_object_name = 'products'
 
 class ProductCreate(CreateView):
     form_class = ProductForm
     template_name = 'product_form.html'
+    success_url = reverse_lazy('Product-list')
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('Product-list')
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    template_name = 'product_update.html'
+    model = Product
     success_url = reverse_lazy('Product-list')
