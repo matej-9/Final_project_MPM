ecf836e342c8f3d2f16cec391bacf82ebdcd94ce
Patrik <pato1.polak@gmail.com>
2025-06-08 17:21:59 +0200

    add_to_cart + product button

--- online_store/cart/views.py
+++ online_store/cart/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import ListView
 
 from cart.models import CartItem
+from product.models import Product
 
 
 class Cart(ListView):
+
     model = CartItem
     template_name = 'cart.html'
     context_object_name = 'cart'
 
 
+def add_to_cart(request):
+    product_id=request.POST.get('product_id')
+    product = get_object_or_404(Product, pk=product_id)
+    CartItem.objects.get_or_create(product=product)
+
+
+
+    return redirect("cart")
