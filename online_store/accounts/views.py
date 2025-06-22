43671ebbc3828f4c8d9abc32bce1dcb5fe8cbbc4
Martina-Sablova <sablova.martina@gmail.com>
2025-06-22 21:53:50 +0200

    accounts views.py PEP8 - pylint, black, reset password change text SK to EN,

--- online_store/accounts/views.py
+++ online_store/accounts/views.py
@@ -1,3 +1,7 @@
+"""
+Views for the accounts app: login, signup, logout, profile.
+"""
+
 from django.contrib.auth.forms import AuthenticationForm
 from accounts.forms import SignUpForm
 from django.contrib.auth import logout
@@ -6,47 +10,61 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import views as auth_views
-
 from accounts.models import Profile
 
 def accounts_landing(request):
+    """
+    View to render landing page with signup and login forms.
+    """
     signup_form = SignUpForm()
     login_form = AuthenticationForm()
-    return render(request, 'accounts/landing_both.html', {
-        'signup_form': signup_form,
-        'login_form': login_form,
-    })
-
+    return render(
+        request,
+        "accounts/landing_both.html",
+        {
+            "signup_form": signup_form,
+            "login_form": login_form,
+        },
+    )
 
 class SignUpView(CreateView):
+    """
+    View to handle user signup with SignUpForm.
+    """
     form_class = SignUpForm
-    template_name = 'form.html'
-    success_url = reverse_lazy('home')
+    template_name = "form.html"
+    success_url = reverse_lazy("home")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['view_name'] = 'SignUpView'
+        context["view_name"] = "SignUpView"
         return context
-
 
 class MyLoginView(LoginView):
-    template_name = 'form.html'
-    success_url = reverse_lazy('home')
+    """
+    Custom login view using Django's LoginView.
+    """
+    template_name = "form.html"
+    success_url = reverse_lazy("home")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['view_name'] = 'MyLoginView'
+        context["view_name"] = "MyLoginView"
         return context
 
-
+@login_required(login_url="/accounts/login/")
 def user_logout(request):
+    """
+    Log out the user and redirect back to referring page or home.
+    """
     logout(request)
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+    return redirect(request.META.get("HTTP_REFERER", "/"))
 
-
-@login_required(login_url='/accounts/login/')
+@login_required(login_url="/accounts/login/")
 def profile_view(request):
+    """
+    View to display and create user profile if not exists.
+    """
     user = request.user
-    profile, created = Profile.objects.get_or_create(user=user)
-    return render(request, 'profile.html', {'profile': profile})
+    profile, _ = Profile.objects.get_or_create(user=user) # pylint: disable=no-member
+    return render(request, "profile.html", {"profile": profile})
